1e1443c594c01320731206614638ff7254369a8a
PulpaMaster <email@example.com>
2025-01-08 19:36:12 +0100

    DB

---
+++ src/database.ts
@@ -0,0 +1,6 @@
+import 'dotenv/config'
+import { drizzle, NodePgDatabase } from 'drizzle-orm/node-postgres'
+import * as schema from './db/schema'
+type DBSchema = NodePgDatabase<typeof schema>
+const db: DBSchema = drizzle(process.env.DATABASE_URL!)
+export { db }
---
+++ src/db/schema.ts
@@ -0,0 +1,16 @@
+import type { password } from 'bun'
+import { integer, pgTable, varchar } from 'drizzle-orm/pg-core'
+
+export const postsTable = pgTable('posts', {
+ id: integer().primaryKey().generatedAlwaysAsIdentity(),
+ text: varchar({ length: 255 }).notNull(),
+ userId: integer()
+    .notNull()
+    .references(()=>usersTable.id,{onDelete: 'cascade'}),
+})
+
+export const usersTable = pgTable('users', {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    username: varchar({ length: 255 }).notNull().unique(),
+    password: varchar({ length: 255 }).notNull(),
+   })
