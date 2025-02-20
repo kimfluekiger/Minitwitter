9b4b67206037c09933dfe11cf0ce989a37ece9ab
Kilian <-->
2025-02-20 19:20:45 +0100

    Auth Fix & Post change + Delete

---
+++ frontend/components/LogoutButton.vue
@@ -0,0 +1,16 @@
+<template>
+    <button @click="logout" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none">
+      Login
+    </button>
+  </template>
+  
+  <script setup lang="ts">
+  import { useRouter } from 'vue-router'
+  
+  const router = useRouter()
+  
+  const logout = () => {
+    localStorage.removeItem('token')
+    router.push('/login')
+  }
+  </script>
