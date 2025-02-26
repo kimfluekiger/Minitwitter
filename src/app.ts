95b392543109ff82431a9a591b5dcd4dde78877b
kim.fluekiger <kim.fluekiger@edu.teko.ch>
2025-02-26 19:19:10 +0100

    redis

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import express, { type Request, type Response } from 'express'
 import { initializeAPI } from './api'
 import cors from 'cors'
+import { initializeMessageBroker } from './message-broker'
+
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 const Port = 3000
 
+initializeMessageBroker()
 
 initializeAPI(app)
 
