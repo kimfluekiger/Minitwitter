48123201cd038c85b09e47b1a86f4443c587811f
n.celik <n.celik@gmx.ch>
2025-03-03 23:19:21 +0100

    Pub/Sub Verbindung und Rollen

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,23 @@ import { initializeAPI } from './api'
 import cors from 'cors'
 import { initializeMessageBroker } from './message-broker'
 
+const SERVER_ROLE = process.env.SERVER_ROLE || 'all'
+const allowedServerRoles = ['all', 'api', 'worker']
+if (!allowedServerRoles.includes(SERVER_ROLE)) {
+    console.error(`Invalid SERVER_ROLE: ${SERVER_ROLE}`)
+    process.exit(1)
+}
 
+// For the worker server & apii queue
+initializeMessageBroker()
+
+
+// For the API server
+if (SERVER_ROLE === 'all' || SERVER_ROLE === 'api') {
 const app = express()
 app.use(express.json())
 app.use(cors())
 const Port = 3000
-
-initializeMessageBroker()
 
 initializeAPI(app)
 
@@ -17,3 +27,4 @@ app.listen(Port, (err) => {
             if (err) console.log(err);
         else console.log("Server listening on PORT", Port);
     });
+}
